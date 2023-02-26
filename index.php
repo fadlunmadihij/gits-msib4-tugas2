1c93ec48e56ec763274c0a0de859c1a62311c023
fadlunmadihij <fadlunmadihij@gmail.com>
2023-02-26 12:00:17 +0700

    fdln commit

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Fashion Wanita</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Feather Icons -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <!-- My Style -->
+
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+    <!-- Navbar start -->
+    <nav class="navbar">
+      <a href="#" class="navbar-logo">fashion<span>wanita</span>.</a>
+
+      <div class="navbar-nav">
+        <a href="#home">Home</a>
+        <a href="#about">Tentang Kami</a>
+        <a href="#menu">Menu</a>
+        <a href="#contact">Kontak</a>
+      </div>
+
+      <div class="navbar-extra">
+        <a href="#" id="search"><i data-feather="search"></i></a>
+        <a href="#" id="shopping-cart"><i data-feather="shopping-cart"></i></a>
+        <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
+      </div>
+    </nav>
+    <!-- Navbar end -->
+
+    <!-- Hero Section start -->
+    <section class="hero" id="home">
+      <main class="content">
+        <h1>Sempurnakan <span>Penampilanmu</span></h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, natus.
+        </p>
+        <a href="#" class="cta">Beli Sekarang</a>
+      </main>
+    </section>
+
+    <!-- Hero Section end -->
+
+    <!-- About Section start -->
+    <section id="about" class="about">
+      <h2><span>Tentang</span> Kami</h2>
+
+      <div class="row">
+        <div class="about-img">
+          <img src="img/tentang-kami.jpg" alt="Tentang Kami" />
+        </div>
+        <div class="content">
+          <h3>Kenapa memilih toko kami?</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio quae
+            soluta beatae laudantium odit voluptatem.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo ad
+            omnis ipsa odit veritatis sequi odio necessitatibus atque molestiae.
+            Laudantium.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <!-- About Section end -->
+
+    <!-- Menu Section start -->
+    <section id="menu" class="menu">
+      <h2><span>Menu</span>Kami</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+        voluptatibus eveniet dolor beatae eum odio?
+      </p>
+
+      <div class="row">
+        <div class="memu-card">
+          <img
+            src="img/menu/2.jpg"
+            alt="Tas cewek"
+            class="menu-card-img"
+            style="border-radius: 50%"
+          />
+          <h3 class="memu-card-title">-Tas Cewek-</h3>
+          <p class="memu-card-price">IDR 250K</p>
+        </div>
+        <div class="memu-card">
+          <img
+            src="img/menu/2.jpg"
+            alt="Tas cewek"
+            class="menu-card-img"
+            style="border-radius: 50%"
+          />
+          <h3 class="memu-card-title">-Tas Cewek-</h3>
+          <p class="memu-card-price">IDR 250K</p>
+        </div>
+        <div class="memu-card">
+          <img
+            src="img/menu/2.jpg"
+            alt="Tas cewek"
+            class="menu-card-img"
+            style="border-radius: 50%"
+          />
+          <h3 class="memu-card-title">-Tas Cewek-</h3>
+          <p class="memu-card-price">IDR 250K</p>
+        </div>
+        <div class="memu-card">
+          <img
+            src="img/menu/2.jpg"
+            alt="Tas cewek"
+            class="menu-card-img"
+            style="border-radius: 50%"
+          />
+          <h3 class="memu-card-title">-Tas Cewek-</h3>
+          <p class="memu-card-price">IDR 250K</p>
+        </div>
+        <div class="memu-card">
+          <img
+            src="img/menu/2.jpg"
+            alt="Tas cewek"
+            class="menu-card-img"
+            style="border-radius: 50%"
+          />
+          <h3 class="memu-card-title">-Tas Cewek-</h3>
+          <p class="memu-card-price">IDR 250K</p>
+        </div>
+        <div class="memu-card">
+          <img
+            src="img/menu/2.jpg"
+            alt="Tas cewek"
+            class="menu-card-img"
+            style="border-radius: 50%"
+          />
+          <h3 class="memu-card-title">-Tas Cewek-</h3>
+          <p class="memu-card-price">IDR 250K</p>
+        </div>
+      </div>
+    </section>
+    <!-- Menu Section end -->
+
+    <!-- Contact Section start -->
+    <section id="contact" class="contact">
+      <h2><span>Kontak</span>Kami</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+        neque.
+      </p>
+      <div class="row">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63268.71418339819!2d112.87007498971894!3d-7.651433837724233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7c5e1a5fb57df%3A0xbe8c958dde768096!2sPasuruan%2C%20Kota%20Pasuruan%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1677231248479!5m2!1sid!2sid"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          class="maps"
+        ></iframe>
+
+        <form action="">
+          <div class="input-group">
+            <i data-feather="user"></i>
+            <input type="text" placeholder="nama" />
+          </div>
+          <div class="input-group">
+            <i data-feather="mail"></i>
+            <input type="text" placeholder="email" />
+          </div>
+          <div class="input-group">
+            <i data-feather="phone"></i>
+            <input type="text" placeholder="no hp" />
+          </div>
+          <button type="submit" class="btn">kirim pesan</button>
+        </form>
+      </div>
+    </section>
+    <!-- Contact Section end -->
+
+    <!-- Footer start -->
+    <footer>
+      <div class="socials">
+        <a href="#"><i data-feather="instagram"></i></a>
+      </div>
+      <div class="links">
+        <a href="#home">Home</a>
+        <a href="#about">Tentang Kami</a>
+        <a href="#menu">Menu</a>
+        <a href="#contact">Kontak</a>
+      </div>
+
+      <div class="credit">
+        <p>Created by <a href="">FadlunMadihij</a>. | &copy; 2023.</p>
+      </div>
+    </footer>
+    <!-- Footer end -->
+
+    <!-- Feather Icons -->
+    <script>
+      feather.replace();
+    </script>
+
+    <!-- My javascript -->
+
+    <script src="js/script.js"></script>
+  </body>
+</html>
